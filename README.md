c075810c94282eee7037314eba5faa8687c7acc8
Manu160296 <fmanuela499@gmail.com>
2017-12-25 18:19:21 -0500

    ejercicio finalizado

---
+++ README.md
@@ -0,0 +1,24 @@
+# CLASE PRÁCTICA :
+
+* **Track:** _Common Core_
+* **Curso:** _Agiliza tu desarrollo_
+* **Unidad:** _Crea tu propia red social_
+
+***
+## Descripción del proyecto:
+Vamos a crear un carrusel de imágenes!
+![IMAGEN-1](https://raw.githubusercontent.com/AnaSalazar/curricula-js/b8605ade7fc7b6b9093f25d37d1bcdf60c6f1888/04-social-network/02-jquery/07-guided-exercises/carrusel.png)
+![IMAGEN-2](https://raw.githubusercontent.com/AnaSalazar/curricula-js/b8605ade7fc7b6b9093f25d37d1bcdf60c6f1888/04-social-network/02-jquery/07-guided-exercises/carrusel-2.png)
+
+***
+## Lenguajes utilizados :
+
+~~~
+HTML
+~~~
+~~~
+CSS
+~~~  
+~~~
+JAVASCRIPT
+~~~    
--- js/app.js
+++ js/app.js
@@ -1,41 +1,40 @@
 $(document).ready(loadPage);
-var loadPage = function () {
-    var target =0;
-    // elementos:
-    var $buttons = $('.control');
-    console.log($buttons)
-    var $previous = $('.previous');
-    var $next = $('.next');
-    //eventos
-    $buttons.click(changeImage);
-    $previous.click(previousImage);
-    $next.click(nextImage);
+var target = 0;
+var loadPage = function() {
+  // elementos:
+  var $buttons = $('.control');
+  var $previous = $('.previous');
+  var $next = $('.next');
+  // eventos
+  $buttons.click(changeImage);
+  $previous.click(previousImage);
+  $next.click(nextImage);
+};
 
-    var changeImage = function () {
-        console.log('funciona')
-       target = parseInt($(this).data(target)) ;
-       showImge(target);
-    }
+var changeImage = function() {
+  target = parseInt($(this).data('target')) ;
+  showImage(target);
+};
 
-    var previousImage = function(e) {
-        e.preventDefault();
-        target = target - 1;
-        target = (target < 0) ? 4 :target;
-        showImge(target);
-    }
+var previousImage = function(event) {
+  event.preventDefault();
+  target = target - 1;
+  target = (target < 0) ? 4 : target;
+  showImage(target);
+};
 
-    var nextImage = function (e) {
-        e.preventDefault();
-        target = target + 1 ;
-        target = (target>4) ? 0 : target;
-        showImge(target);
-    }
+var nextImage = function(event) {
+  event.preventDefault();
+  target = target + 1 ;
+  target = (target > 4) ? 0 : target;
+  showImage(target);
+};
 
-    var showImge = function () {
-       var $lastSlide = $('div.active') ;
-       var $slide = $("'div[data-slide='" + target + " ']");
-       $lastSlide.removeClass("active");
-       $slide.addClass('active');
-    }
-} 
+var showImage = function(target) {
+  var $lastSlide = $('div.active') ;
+  var $slide = $("div[data-slide='" + target + "']");
+  $lastSlide.removeClass('active');
+  $slide.addClass('active');
+};
+
 loadPage();
